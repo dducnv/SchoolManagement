6f7bd7d7e958e5eb06827f3cc9b43286ea0841ad
Nguyen Van Duc <ducbe2k2@gmail.com>
2022-03-19 09:48:50 +0700

    model

---
+++ SchoolManagement/Models/SessionDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagement.Models
+{
+    public class SessionDetails
+    {
+        public int id { get; set; }
+        public int ss_id { get; set; }
+        public string date_start { get; set; }
+        public int status { get; set; }
+    }
+}
